e4657c8fb8220ebeabef2340dd81cc3e967714b3
Tim <tim-script@posteo.com>
2024-12-09 10:23:11 +0100

    Add files

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Sanctum</title>
+        @vite(['resources/css/app.css', 'resources/js/app.ts'])
+    </head>
+    <body>
+        <div id="app"></div>
+    </body>
+</html>
