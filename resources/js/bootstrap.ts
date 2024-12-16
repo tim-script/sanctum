92ad121429c757321f6a4bc552ea8c7b24170ab2
Tim <tim-script@posteo.com>
2024-12-16 10:32:02 +0100

    bootstrap.ts: fix TypeScript errors

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -1,4 +1,11 @@
-import axios from 'axios';
+import axios, { Axios } from 'axios';
+
+declare global {
+    interface Window {
+        axios: Axios
+    }
+}
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
